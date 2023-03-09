40b14f091a9a20439091d3feee3ac12e1474ab6a
Daniel Rabl <DanielRabl1999@gmail.com>
2023-03-09 16:44:08 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 ﻿#include <qpl/qpl.hpp>
-#include "console.hpp"
 
 struct main_state : qsf::base_state {
+	void add_random() {
+		auto dim = qpl::vec(80, 100);
+
+		qpl::styled_string<qpl::u32_string> new_string;
+
+		for (qpl::size i = 0u; i < dim.y; ++i) {
+			if (qpl::random_b(0.1)) {
+				new_string << qpl::get_random_color();
+			}
+			new_string << qpl::get_random_lowercase_uppercase_number_string(qpl::random(dim.x / 2.0, dim.x * 1.5)) << '\n';
+		}
+		this->console.add(new_string);
+	}
+
 	void init() override {
 		this->console.set_font("consola");
 
@@ -9,7 +22,8 @@ struct main_state : qsf::base_state {
 
 		this->call_on_resize();
 
-		this->console.add_random();
+		this->add_random();
+		this->console.set_input_color(qpl::aqua);
 		this->console.start_accepting_input();
 	}
 	void call_on_resize() override {
@@ -19,19 +33,28 @@ struct main_state : qsf::base_state {
 	void updating() override {
 		this->update(this->console);
 
-		//if (this->console.enter_pressed) {
-		//	this->console.stop_accepting_input();
-		//}
+		if (this->console.line_entered) {
+			qpl::println("text = \"", this->console.get_last_input_line(), "\"");
+			if (this->console.get_last_input_line() == L"func") {
+				this->console.stop_accepting_input();
+			}
+		}
+
 		if (this->event().key_pressed(sf::Keyboard::Tab)) {
 			this->console.start_accepting_input();
 		}
+		if (this->event().key_pressed(sf::Keyboard::F)) {
+			qpl::println("string length = ", qpl::big_number_string(this->console.string_and_input.string().length()));
+		}
+
+
 	}
 	void drawing() override {
 
 		this->draw(this->console);
 	}
 
-	console console;
+	qsf::console console;
 };
 
 int main() try {
