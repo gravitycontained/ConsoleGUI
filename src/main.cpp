df7af1b58fb8e782ae01e0c3eca656376d3c233a
DanielRabl <danielrabl1999@gmail.com>
2023-03-19 13:03:19 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,73 @@
 ﻿#include <qpl/qpl.hpp>
 
+struct console_state : qsf::base_state {
+
+	void add_random() {
+		auto dim = qpl::vec(50, 1);
+
+		qpl::styled_string<qpl::u32_string> new_string;
+
+		for (qpl::size i = 0u; i < dim.y; ++i) {
+			if (qpl::random_b(0.1)) {
+				new_string << qpl::get_random_color();
+			}
+			new_string << qpl::get_random_lowercase_uppercase_number_string(qpl::random(dim.x / 2.0, dim.x * 1.5));
+		}
+		this->console.add(new_string);
+	}
+
+	void init() override {
+		this->console.set_font("consola");
+		this->console.set_unicode_font("unifont");
+		this->console.set_border_texture(qsf::get_texture("border"));
+
+		this->clear_color = qpl::rgb(12, 12, 12);
+
+		this->call_on_resize();
+
+		this->add_random();
+
+		this->console.set_input_color(qpl::aqua);
+		this->console.start_accepting_input();
+	}
+	void call_on_resize() override {
+		this->console.set_dimension(this->dimension());
+	}
+
+	void updating() override {
+		this->update(this->console);
+
+		if (this->console.text_entered) {
+			qpl::println("cursor_pos = ", this->console.cursor_position);
+		}
+		if (this->console.line_entered) {
+
+			auto input = this->console.get_last_input_line();
+			qpl::println("text = \"", input, "\"");
+			if (input == L"func") {
+
+				this->console.stop_accepting_input();
+			}
+			this->console.process_text();
+		}
+
+		if (this->event().key_pressed(sf::Keyboard::Tab)) {
+			this->console.start_accepting_input();
+		}
+		if (this->event().key_pressed(sf::Keyboard::F)) {
+			qpl::println("string length = ", qpl::big_number_string(this->console.string_and_input.string().length()));
+		}
+
+
+	}
+	void drawing() override {
+
+		this->draw(this->console);
+	}
+
+	qsf::console2 console;
+};
+
 struct main_state : qsf::base_state {
 	void add_random() {
 		auto dim = qpl::vec(80, 100);
@@ -73,7 +141,7 @@ struct main_state : qsf::base_state {
 		this->call_on_resize();
 
 		this->add_random();
-		this->add_count_up();
+		//this->add_count_up();
 		this->console.set_input_color(qpl::aqua);
 		this->console.start_accepting_input();
 	}
@@ -121,7 +189,8 @@ int main() try {
 	framework.add_texture("border", "resources/border.png");
 	framework.set_dimension({ 1400u, 950u });
 
-	framework.add_state<main_state>();
+	//framework.add_state<main_state>();
+	framework.add_state<console_state>();
 	framework.game_loop();
 }
 catch (std::exception& any) {
